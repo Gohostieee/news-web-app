d538d086b388ffc3247d440fa1db993d9ef24893
Gohostieee <joshua32eb@gmail.com>
2022-01-28 21:45:27 -0400

    updated ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
+import waves from './waves-1.svg';
+import wavesBody from './waves-body-2.svg'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+          <img src={waves} className='flip wave' alt="logo" />
+
+          <header className="App-header">
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <body>
+        <img src={wavesBody} className='background-image' alt="logo" />
+
+        <div className="circle" id="lowerleft">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        </body>
+      </div>
   );
 }
 
