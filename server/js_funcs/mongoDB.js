36d5a3b9f2d84a900a7db08ff806110841bdb98a
gohostieee <joshua32eb@gmail.com>
2022-02-11 10:12:28 -0400

    oops

--- server/js_funcs/mongoDB.js
+++ server/js_funcs/mongoDB.js
@@ -2,19 +2,6 @@ import { MongoClient } from 'mongodb'
 import  mongoose  from 'mongoose'
 import '../models/articles.js'
 
-const uri = "mongodb+srv://gohost:<password>@cluster0.hhjux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "";
 mongoose.connect(uri)
     .then(console.log("connected and stuff"))
-/*
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
- client.connect(err => {
-    const collection = client.db("test").collection("Article queries");
-    console.log(collection)
-     const p = collection.insertOne(personDocument);
-
-     // perform actions on the collection object
-});
-
-const db = await client.db("Cluster0")
-console.log("free canada")
-*/
