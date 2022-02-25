9ef5fbda2d5098498d47bbc7bae0ecffc2dc7501
sidewinder-IX <sidewinder@programmer.net>
2022-02-25 01:37:23 -0500

    added db function, environment variable, and .gitignore entry

--- server/js_funcs/mongoDB.js
+++ server/js_funcs/mongoDB.js
@@ -5,3 +5,11 @@ import '../models/articles.js'
 const uri = "";
 mongoose.connect(uri)
     .then(console.log("connected and stuff"))
+
+async function articles_store(data) {
+    if (data.status == 'ok') {
+        articles.insertMany(data.articles);
+    } else {
+        throw `${data.code}: ${data.message}`;
+    }
+}
--- server/js_funcs/news-api.js
+++ server/js_funcs/news-api.js
@@ -3,9 +3,10 @@ import  articles  from '../models/articles.js'
 import 'dotenv/config'
 const key=process.env.region
 console.log(key)
+const keyword = process.env.keyword
 async function data_fetch(date) {
     let data;
-    data = await fetch(`https://newsapi.org/v2/everything?pageSize=100&apiKey=${key}`).then((e)=>{
+    data = await fetch(`https://newsapi.org/v2/everything?q=${keyword}&pageSize=100&apiKey=${key}`).then((e)=>{
         return e.json()
     }).then((r) =>{
         return r;
